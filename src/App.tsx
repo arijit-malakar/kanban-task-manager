54aeb45661d148aec39d242448135f3c57d33c2a
Arijit Malakar <arijitmalakar1996@gmail.com>
2024-01-02 21:25:38 +0530

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import GlobalStyles from "./styles/GlobalStyles";
+
+const App = () => {
+  return (
+    <>
+      <GlobalStyles />
+      <div>Kanban Task Management</div>
+    </>
+  );
+};
+
+export default App;
