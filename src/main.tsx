49e6575d1e31dd8df2c24880fd38ee804a146835
Arijit Malakar <arijitmalakar1996@gmail.com>
2024-01-14 18:11:30 +0530

    Revert changes: strict mode

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import debounce from "debounce";
@@ -13,9 +13,9 @@ store.subscribe(
 );
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <App />
-  </Provider>
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
 );
